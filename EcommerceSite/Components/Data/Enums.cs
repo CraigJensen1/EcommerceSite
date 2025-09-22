f815d3eb90911d733d18deb511aee70f853889ad
craig <178933954+CraigJensen1@users.noreply.github.com>
2025-09-22 16:03:17 -0600

    can I push to master

---
+++ EcommerceSite/Components/Data/Enums.cs
@@ -0,0 +1,16 @@
+﻿
+namespace EcommerceSite.Enums
+{  
+    public enum Roles
+    {
+        Admin = 1,
+        Employee,
+        User
+    }
+    public enum Permissions
+    {      
+        EditProducts = 1,
+        EditCategories,
+        EditUsers
+    }    
+}
---
+++ EcommerceSite/Components/Pages/Checkout.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace EcommerceSite.Pages;
+
+public partial class Checkout : ComponentBase
+{
+    private int currentCount { get; set; } = 1;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
